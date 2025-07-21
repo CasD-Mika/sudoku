8d4033da43e33cf54f41221ba3bd2d1837635c57
CasD-Mika <mika.fuisting@gmx.de>
2025-07-21 16:00:29 +0200

    feat: add pwa support for offline usage

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,16 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { GameService } from './shared/services/game.service';
 import { DialogService } from './core/services/dialog.service';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     GameService,
-    DialogService
+    DialogService,
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ]
 };
